445ce9af172e9699a3a0eb0ade7636f44ae4cb9a
bnayaunger <132574914+bnayaunger@users.noreply.github.com>
2024-11-11 18:25:30 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Airbnb-project
+# EDA-project
+Group: Bnaya Unger , Dean Israel, Neta Edri , Talya Coperman .
+This project was made as part as data analysis course in Ariel University. 
+all the rights reserve to the names above 
+*** please download all the filles move it to the same path and open the file names "AirBnB Project.pynb IN the jupiterlab. *** 
---
+++ airbnb quiz.py
@@ -0,0 +1,17 @@
+print('welcome! please fill the quizz and see the results')
+
+a = input("Enter the city:")
+b = input("How many people:")
+c = input("Your trip is in the weekday or weekend?")
+d = input("would you like to stay close to the center  , middle or far?")
+
+image_path = './apt1.jpg'
+image = Image(image_path)
+display(image)
+#f = input("")
+#g = input("")
+#h = input("")
+#i = input("")
+#j = input("")
+
+print('We try our best to give you the best room , Wait for your results')
